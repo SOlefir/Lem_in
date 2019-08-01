de8f065f5ed95f49d6e9eb6dee8569b31df86ccb
Sofiaa OLEFIR <solefir@e1r11p6.unit.ua>
2019-08-01 20:44:19 +0300

    debug

--- src/validation_dop.c
+++ src/validation_dop.c
@@ -6,7 +6,7 @@
 /*   By: solefir <solefir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/01 15:01:18 by solefir           #+#    #+#             */
-/*   Updated: 2019/08/01 18:48:53 by solefir          ###   ########.fr       */
+/*   Updated: 2019/08/01 20:38:19 by solefir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,32 @@
 
 static _Bool	room_in_links(t_list *head)
 {
-	while (head != NULL)
+	while (head != NULL && head->next != NULL && head->next->next != NULL)
 	{
-		if (is_link((char *)head->content &&
-		is_room((char *)head->next->content) &&
-		is_link((char *)head->next->next->content))
+		if (is_link((char*)head->content) &&
+		is_room((char*)head->next->content) &&
+		is_link((char*)head->next->next->content))
 			return (1);
 		head = head->next;
 	}
+	if (head != NULL && head->next != NULL &&
+		is_link((char*)head->content) && is_room((char*)head->next->content))
+		return (1);
 	return (0);
 }
 
 static _Bool	link_in_rooms(t_list *head)
 {
-	while (head != NULL)
+	while (head != NULL && head->next != NULL && head->next->next != NULL)
 	{
-		if (is_room((char *)head->content &&
-			is_link((char *)head->next->content) &&
-			is_room((char *)head->next->next->content))
+		if (is_room((char*)head->content) &&
+			is_link((char*)head->next->content) &&
+			is_room((char*)head->next->next->content))
 			return (1);
 		head = head->next;
 	}
+	if (head != NULL && is_link((char*)head->next->content) &&
+	    is_room((char*)head->content))
 	return (0);
 }
 
@@ -55,12 +60,12 @@ _Bool			last_validation(t_list *head)
 			head = head->next;	
 		while (temp != NULL)
 		{
-			if (is_link(temp->content))
-				if (ft_strnstr((char*)head->content, temp->content,
+			if (is_link((char*)temp->content))
+				if (ft_strnstr((char*)head->content, (char*)temp->content,
 					head->content_size) == NULL)
 					if ((g_error_nbr = 8))
 						return (0);
-			temp = head->next;
+			temp = temp->next;
 		}
 		head = head->next;
 	}
